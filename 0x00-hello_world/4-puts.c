a11141119231325427044b8773cebd22903638dc
ahmed <ahelmyeng1@gmail.com>
2023-07-13 14:06:16 -0700

    task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Function utilized to do the code
+ *
+ * Description: Utilizing puts function
+ *
+ * Return: Zero as the output is a message
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
