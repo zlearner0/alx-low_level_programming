d8602596d94040deeadcc2390b697d3fd929ed2f
ahmed <ahelmyeng1@gmail.com>
2023-07-13 14:28:48 -0700

    task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Utilizing main function to do the coe
+ *
+ * Descripion: Using printf for the output
+ *
+ * Return: Zero as the output is printed text
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
