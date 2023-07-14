3ef5b64cb8586bc394411192d0516f13cb802828
ahmed <ahelmyeng1@gmail.com>
2023-07-14 07:20:59 -0700

    task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Utilizing main function to do the code
+ *
+ * Descripion: Getting the alphabet in lowercase
+ *
+ * Return: Zero depending on printing values
+*/
+
+int main(void)
+{
+int i;
+for (int i = 0; i < 16; i++)
+{
+if (i < 10)
+{
+putchar(i + '0');
+}
+else
+{
+putchar(i - 10 + 'A');
+}
+}
+putchar('\n');
+return (0);
+}
