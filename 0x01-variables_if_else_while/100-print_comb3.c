1ac310d1da0dc0a433b7b56133af48fb6894f65b
ahmed <ahelmyeng1@gmail.com>
2023-07-14 10:36:27 -0700

    task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Utilizing main function to do the code
+ *
+ * Descripion: Getting the numbers from 01 to 99 with comma
+ *
+ * Return: Zero depending on printing values
+*/
+
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i < 10; i++)
+{
+
+for (j = 1; j < 10; j++)
+{
+if (j >= i)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i + '0' != '9' || j + '0' != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+
+}
+}
+
+putchar('\n');
+return (0);
+}
