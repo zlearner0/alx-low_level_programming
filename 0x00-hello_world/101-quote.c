add9334c8643be7ba2b358726554d200896ddc2f
ahmed <ahelmyeng1@gmail.com>
2023-07-13 23:13:56 -0700

    task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Utilizing main function to do the code
+ *
+ * Descripion: Using write function to print the output
+ *
+ * Return: 1 as required
+*/
+
+int main(void)
+{
+char solu[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, resolve, 59);
+return (1);
+}
