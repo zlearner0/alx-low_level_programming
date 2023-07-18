53e005160b4b2db18a98749c98aa36dc565996b0
ahmed <ahelmyeng1@gmail.com>
2023-07-18 12:01:36 -0700

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,24 @@
 #include "main.h"
-#include <unistd.h>
 
-int _putchar3(char letter)
+/**
+ * main - Utilizing main function to do the code
+ *
+ * Descripion: printing the string
+ *
+ * Return: Zero depending on printing values
+*/
+
+int main(void)
 {
-return (write(1, &letter, 1));
+int i = 0;
+char text[] = "_putchar";
+while (text[i] != '\0')
+{
+char s = text[i];
+_putchar(s);
+i++;
+}
+char n = '\n';
+_putchar(n);
+return (0);
 }
