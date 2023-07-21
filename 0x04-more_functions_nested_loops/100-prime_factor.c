7b8f15433ebcb6e9f24505c782e1fefbc0e3e7c5
ahmed <ahelmyeng1@gmail.com>
2023-07-21 01:13:03 -0700

    task

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Utilizing main function to do the code
+ *
+ * Descripion: Using printf for the output
+ *
+ * Return: Zero as the output is printed text
+*/
+
+int main(void)
+{
+int prime;
+prime = 1231952;
+int i;
+i = 2;
+while (i <= prime)
+{
+if (prime % i == 0 && prime != i)
+{
+prime = prime / i;
+}
+else
+{
+i++;
+}
+}
+printf("%i\n", prime);
+return (0);
+}
