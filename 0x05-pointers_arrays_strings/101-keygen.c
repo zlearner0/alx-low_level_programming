7a78140a3223c43d508d35c05f198af51e75bba1
ahmed <ahelmyeng1@gmail.com>
2023-07-25 12:11:21 -0700

    task

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Utilizing main function to do the code
+ * Return: 0 if right
+*/
+
+int main(void)
+{
+int n;
+srand(8745269722257);
+n = rand();
+printf("%d\n", n);
+return (0);
+}
