a5f9b8ec6c9c552acce038d0d4e554e2543e5e33
ahmed <ahelmyeng1@gmail.com>
2023-07-26 09:36:41 -0700

    task

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts string to uppercase
+ * @str: lower case string to be converted to uppercase
+ * Return: a string of uppercase
+*/
+
+char *string_toupper(char *str)
+{
+int i = 0;
+while (str[i])
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] -= 32;
+i++;
+}
+return (str);
+}
