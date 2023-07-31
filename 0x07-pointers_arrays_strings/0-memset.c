5a7a16da59db43fbb3d3a22dbfcef452a6d43113
ahmed <ahelmyeng1@gmail.com>
2023-07-31 09:19:04 -0700

    task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fill the memory with a character b till n number
+ * @s: pointer to string character array
+ * @b: the character will be repeated and filled
+ * @n: the numbers of repatation for character b
+ * Return: pointer to string array
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+char *p = s;
+while (n--)
+{
+*p = b;
+p++;
+}
+*p = '\0';
+return (s);
+}
