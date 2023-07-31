d271fe01049ca808463593981ba21d6207683ecf
ahmed <ahelmyeng1@gmail.com>
2023-07-31 09:34:09 -0700

    task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,9 +9,11 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-for (unsigned int i = 0; i < n; i++)
+unsigned int i;
+for (i = 0; i < n; i++)
 {
 *(s + i) = b;
 }
+*(s + i) = '\0';
 return (s);
 }
