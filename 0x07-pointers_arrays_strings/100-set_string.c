79dcd0d115154872b8f816db11984814b7315f9f
ahmed <ahelmyeng1@gmail.com>
2023-07-31 13:03:38 -0700

    task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - g sets the value of a pointer to apointer to  a char
+ * @s: pointer to array to be switched
+ * @to: the pointer will be go to the s pointer
+ * Return: nothing
+*/
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
