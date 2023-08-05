dcaac8f40619e69beada1bbc23c8ca228bda20c9
ahmed <ahelmyeng1@gmail.com>
2023-08-05 01:37:05 -0700

    task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Utilizing main function to do the code
+ * @argc: number of argv array elements
+ * @argv: string array elements filled with command line
+ * Return: Zero as the output is printed text
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
