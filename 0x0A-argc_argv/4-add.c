2ffeb6a2707ae018e7c09f254026c6bb3027b4df
ahmed <ahelmyeng1@gmail.com>
2023-08-05 03:18:30 -0700

    task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <ctype.h>
+/**
+ * main - Utilizing main function to do the code
+ * @argc: number of argv array elements
+ * @argv: string array elements filled with command line
+ * Return: Zero as the output for success and 1 if failed
+*/
+
+int main(int argc, char *argv[])
+{
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+int sum = 0;
+for (int i = 1; i < argc; i++)
+{
+for (int j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+return (0);
+}
+
