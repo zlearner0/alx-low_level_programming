eef018bf1ac3dc436acb0610620552232a4b83a5
ahmed <ahelmyeng1@gmail.com>
2023-08-05 00:59:51 -0700

    task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Utilizing main function to do the code
+ * @argc: number of argv array elements
+ * @argv: string array elements filled with command line
+ * Return: Zero as the output is printed text
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
+
