1dd241d245028ed6c72249781008dab67250d181
ahmed <ahelmyeng1@gmail.com>
2023-08-07 08:45:06 -0700

    task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,6 +3,23 @@
 #include <stdio.h>
 
 /**
+ * _len - get the length of a string
+ * @s: the pointer to a string
+ * Return: the string length
+*/
+
+int _len(char *s)
+{
+int len1 = 0;
+while (*s != '\0')
+{
+len1++;
+s++;
+}
+return (len1);
+}
+
+/**
  * _strdup - ceate a pduplicate for a string
  * @str: the pointer to string to be duplicted
  * Return: pointer to the new string duplicated
