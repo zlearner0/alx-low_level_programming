20c37d0262436aa029013a1a7892fb73d847c5f3
ahmed <ahelmyeng1@gmail.com>
2023-08-10 02:25:44 -0700

    task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,35 +11,37 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *str1, *str2, *p, *start;
-int len1 = 0;
-int len2 = 0;
+unsigned int alllen;
+unsigned int len1 = 0;
+unsigned int len2 = 0;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-str1 = s1;
-str2 = s2;
-while (*str1 != '\0')
+while (s1[len1] != '\0')
 {
 len1++;
-str1++;
 }
-while (*str2 != '\0')
+while (s2[len2] != '\0')
 {
 len2++;
-str2++;
 }
-if (n > len2)
+if (n >= len2)
 n = len2;
-p = malloc(sizeof(char) * (len1 + n + 1));
-if (p == NULL)
+alllen = len1 + n;
+char *result = (char *)malloc((alllen + 1) * sizeof(char));
+if (result == NULL)
+{
 return (NULL);
-start = p;
-while (*s1 != '\0')
-*p++ = *s1++;
-while (n--)
-*p++ = *s2++;
-*p = '\0';
-return (start);
+}
+for (unsigned int i = 0; i < len1; i++)
+{
+result[i] = s1[i];
+}
+for (unsigned int i = 0; i < n; i++)
+{
+result[len1 + i] = s2[i];
+}
+result[alllen] = '\0';
+return (result);
 }
