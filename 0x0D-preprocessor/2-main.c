6956c53ec09a18770ed0782a3872f517b1a56225
ahmed <ahelmyeng1@gmail.com>
2023-08-14 10:07:41 -0700

    task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - run the code inside it
+ * @void: no parameter to input
+ * Return: zero for all cases
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
