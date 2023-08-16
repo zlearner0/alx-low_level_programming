58ffacd3afd8dc20fe152982592dd6304f6b67d6
ahmed <ahelmyeng1@gmail.com>
2023-08-16 11:20:59 -0700

    task

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -11,14 +11,19 @@ int(*get_op_func(char *s))(int, int)
 {
 int i;
 i = 0;
-op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
+op_t ops[6];
+ops[0].op = "+";
+ops[0].f = op_add;
+ops[1].op = "-";
+ops[1].f = op_sub;
+ops[2].op = "*";
+ops[2].f = op_mul;
+ops[3].op = "/";
+ops[3].f = op_div;
+ops[4].op = "%";
+ops[4].f = op_mod;
+ops[5].op = NULL;
+ops[5].f = NULL;
 while (i < 5)
 {
 if (s && s[0] == ops[i].op[0] && !s[1])
