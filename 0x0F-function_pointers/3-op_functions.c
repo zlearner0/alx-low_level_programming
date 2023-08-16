33c5c4ccb306a6231271d3ff93a0c6eee139d652
ahmed <ahelmyeng1@gmail.com>
2023-08-16 10:56:50 -0700

    task

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "3-calc.h"
+
+
+/**
+ * op_add - add two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of operation
+*/
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - subtract two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of operation
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of operation
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - divide two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of operation
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - modulu two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of operation
+*/
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
