c8bcbac893e50007fd6998c04221b2031e50b761
ahmed <ahelmyeng1@gmail.com>
2023-08-25 09:05:12 -0700

    task

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,6 +3,26 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+
+/**
+ * _strlen2 - calculates string length
+ * @str: the input string
+ * Return: string length
+*/
+int _strlen2(const char *str)
+{
+int i;
+if (!str)
+return (0);
+for (i = 0; str[i] != '\0'; i++)
+;
+return (i);
+}
+
+
+
+
+
 /**
  * add_node_end - add a node to single linked list
  * @head: pointer to list of struct data type
@@ -24,7 +44,7 @@ if (!new->str)
 free(new);
 return (0);
 }
-new->len = _strlen(str);
+new->len = _strlen2(str);
 }
 new->next = NULL;
 if (tmp)
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -23,6 +23,6 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 int _strlen(const char *str);
-
+int _strlen2(const char *str);
 
 #endif
