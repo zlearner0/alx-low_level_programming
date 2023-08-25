8152d4889e1540dfdd76a4b0e2bf8fb192e3652e
ahmed <ahelmyeng1@gmail.com>
2023-08-25 09:51:57 -0700

    task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,7 @@
+#include "lists.h"
+void _constructor(void)__attribute__((constructor));
+
+void _constructor(void)
+{
+printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n"); 
+}
