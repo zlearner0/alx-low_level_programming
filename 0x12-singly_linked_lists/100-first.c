b0cebf87a73e568c834904450121e3073ed99082
ahmed <ahelmyeng1@gmail.com>
2023-08-25 09:59:56 -0700

    task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,13 @@
 #include "lists.h"
 void _constructor(void)__attribute__((constructor));
 
+/**
+ * _constructor - initialized before main function
+ * Return: nothing
+*/
+
 void _constructor(void)
 {
-printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n"); 
+printf("(A tortoise, having pretty good sense of a hare's nature");
+printf(", challenges one to a race.)\n");
 }
