f1b16e4a85b2ad8b7170720b4b0fce91fb94d02f
ahmed <ahelmyeng1@gmail.com>
2023-08-28 10:06:52 -0700

    task

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * add_nodeint - add node at list start
+ * @head: pointer to existing node
+ * @n: the int value inside the struct
+ * Return: pointer to newly added point
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *new;
+new = malloc(sizeof(listint_t));
+if (!new)
+{
+return (NULL);
+}
+new->n = n;
+new->next = *head;
+*head = new;
+return (new);
+}
