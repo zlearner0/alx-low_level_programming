f7b36d0f54deb39d6f09f3149838630f3907a771
ahmed <ahelmyeng1@gmail.com>
2023-08-28 13:37:01 -0700

    task

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_listint2 - free struct list
+ * @head: pointer to existing node
+ * Return: nothing
+*/
+
+void free_listint2(listint_t **head)
+{
+listint_t *tmp;
+while (*head != NULL)
+{
+tmp = *head;
+*head = (*head)->next;
+free(tmp);
+}
+}
