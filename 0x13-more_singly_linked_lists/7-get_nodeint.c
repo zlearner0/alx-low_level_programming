1936bdd405f48fe4d58617f4ed44c498ce78dd06
ahmed <ahelmyeng1@gmail.com>
2023-08-29 08:50:20 -0700

    task

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,18 +9,18 @@
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-int count = -1;
+unsigned int count = 0;
 if (head == NULL)
 return (NULL);
 while (head != NULL)
 {
-count++;
 if (count == index)
 {
 return (head);
 }
 head = head->next;
+count++;
 }
-if (index > count)
+/* if (index > count) */
 return (NULL);
 }
