341a30ccb069409a376bf08486f7ed10a0eec548
ahmed <ahelmyeng1@gmail.com>
2023-08-29 09:05:14 -0700

    task

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * insert_nodeint_at_index - insert node in a list
+ * @head: pointer to first node
+ * @idx: the index of new node
+ * @n: the integer data of the new node
+ * Return: pointer to the new node
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+listint_t *new, *starter;
+int count = -1;
+starter = *head;
+if (!n)
+return (NULL);
+new = malloc(sizeof(listint_t));
+if (!new)
+return (NULL);
+new->n = n;
+if (*head == NULL) /* in case no any node */
+{
+	*head = new;
+	new->next = NULL;
+	return (new);
+}
+while (starter != NULL)
+{
+count++;
+if (count == idx && idx == 0) /* add first node */
+{
+new->next = starter;
+*head = new;
+return (new);
+}
+if (count == idx - 1) /* add inner node */
+{
+new->next = starter->next;
+starter->next = new;
+return (new);
+}
+starter = starter->next;
+}
+if (count + 1 == idx) /* add last node */
+{
+starter->next = new;
+new->next = NULL;
+return (new);
+}
+/* if (count + 1 < idx) */
+return (NULL);
+}
