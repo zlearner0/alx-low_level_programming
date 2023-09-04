14f542119b23ee4752f7f3b46372d69b025761fd
ahmed <ahelmyeng1@gmail.com>
2023-09-04 08:44:54 -0700

    task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - read a file to standard output
+ * @filename: the to be read
+ * @letters: no. of letters to be read
+ * Return: no. of letters read
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int f;
+ssize_t num_rd;
+ssize_t num_writ;
+char arr[letters];
+if (filename == NULL)
+return (0);
+f = open(filename, O_RDONLY);
+if (f == -1)
+return (0);
+num_rd = read(f, arr, letters);
+if (num_rd == -1)
+return (0);
+num_writ = write(STDOUT_FILENO, arr, num_rd);
+if (num_writ == -1 || num_writ != num_rd)
+return (0);
+close(f);
+return (num_rd);
+}
