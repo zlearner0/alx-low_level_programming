69042e80042c623c352a2e1b216d0f388c971ae5
ahmed <ahelmyeng1@gmail.com>
2023-09-04 10:22:58 -0700

    task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,41 +2,65 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #define BUFFER_SIZE 1024
-
+#define perm (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+/**
+ * print_error_and_exit - print message error
+ * @msg: error message
+ * @f: the required name of file
+ * @exit_code: the no. of error
+ * Return: nothing
+ */
+void print_error_and_exit(const char *msg, const char *f, int exit_code)
+{
+dprintf(STDERR_FILENO, msg, f);
+dprintf(STDERR_FILENO, "\n");
+exit(exit_code);
+}
 /**
  * main - copies the content of a file to another file
  * @argc: number of arguments
  * @argv: array of arguments
- *
  * Return: 0 on success, otherwise the exit status as requested in the prompt
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-int fd_from, fd_to, n_read, n_written;
+int file_from, file_to, bytes_read, bytes_written;
 char buffer[BUFFER_SIZE];
-mode_t mode = S_IWUSR | S_IRUSR | S_IWGRP | S_IRGRP | S_IROTH;
 if (argc != 3)
-dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]), exit(97);
-fd_from = open(argv[1], O_RDONLY);
-if (fd_from == -1)
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
-if (fd_to == -1)
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-while ((n_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 {
-n_written = write(fd_to, buffer, n_read);
-if (n_written == -1)
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+print_error_and_exit("Usage: cp file_from file_to", "", 97);
 }
-if (n_read == -1)
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-if (close(fd_from) == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
-if (close(fd_to) == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+file_from = open(argv[1], O_RDONLY);
+if (file_from == -1)
+{
+print_error_and_exit("Error: Can't read from file %s", argv[1], 98);
+}
+file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, perm);
+if (file_to == -1)
+{
+print_error_and_exit("Error: Can't write to %s", argv[2], 99);
+}
+while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
+{
+bytes_written = write(file_to, buffer, bytes_read);
+if (bytes_written == -1)
+{
+print_error_and_exit("Error: Can't write to %s", argv[2], 99);
+}
+}
+if (bytes_read == -1)
+{
+print_error_and_exit("Error: Can't read from file %s", argv[1], 98);
+}
+if (close(file_from) == -1)
+print_error_and_exit("Error: Can't close fd %d", argv[1], 100);
+if (close(file_to) == -1)
+{
+print_error_and_exit("Error: Can't close fd %d", argv[2], 100);
+}
 return (0);
 }
