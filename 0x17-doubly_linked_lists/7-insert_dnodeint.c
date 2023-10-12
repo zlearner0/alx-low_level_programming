6334145b124c07e6cc6974076eac6fb40e698a54
ahmed <ahelmyeng1@gmail.com>
2023-10-12 12:35:56 -0700

    task

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -4,49 +4,39 @@
  * insert_dnodeint_at_index - insert a node at index
  * @h: reference to head
  * @idx: specified index
- *@n: added node integer data
+ * @n: added node integer data
  * Return: pointer to added node
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 dlistint_t *new, *current = (*h);
-unsigned int count = 0, isFound = 0;
-current = (*h);
-while (current)
+unsigned int count = 0;
+if (idx == 0)
 {
-if (count == idx - 1)
-{
-isFound = 1;
-break;/*we break at the node just before index*/
+return (add_dnodeint(h, n));
 }
+while (current && count < idx - 1)
+{
 current = current->next;
 count++;
 }
-if (!isFound && count != -1)
+if (!current)
+{
 return (NULL);
+}
 new = malloc(sizeof(dlistint_t));
 if (!new)
+{
 return (NULL);
-if (!(*h)) /*insert first node*/
-{
-new->n = n;
-new->prev = NULL;
-new->next = NULL;
-(*h) = new;
 }
-else if (current->next)/*insert inner node*/
-{
-current = current->next;
-new->n = n;
-new->prev = current->prev;
-new->next = current;
-}
-else/*insert last node*/
-{
 new->n = n;
 new->prev = current;
-new->next = NULL;
+new->next = current->next;
+if (current->next)
+{
+current->next->prev = new;
 }
+current->next = new;
 return (new);
 }
