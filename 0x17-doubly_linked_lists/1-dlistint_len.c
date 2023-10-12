ad6b87a6dcb421757fb7b96a45354dcd273d6b49
ahmed <ahelmyeng1@gmail.com>
2023-10-12 11:17:47 -0700

    task

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - count the list members
+ * @h: the pointer to node
+ * Return: no. of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+if (!h)
+return (0);
+size_t count = 0;
+while (h)
+{
+count++;
+h = h->next;
+}
+return (count);
+}
